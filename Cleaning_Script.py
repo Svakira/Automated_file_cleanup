ef0d9f26ecc648f198ef3878c76da3e537e5a862
Sara Cardona <98909120+Svakira@users.noreply.github.com>
2023-07-24 09:51:38 -0500

    Translated

--- Cleaning_Script.py
+++ Cleaning_Script.py
@@ -2,52 +2,52 @@ import os
 import re
 
 def clean_text(text, regex_list):
-    # Eliminar espacios en blanco al principio y al final del texto
+    # Remove leading and trailing whitespaces from the text
     cleaned_text = text.strip()
     
-    # Eliminar expresiones regulares especificadas en la lista regex_list
+    # Remove specified regular expressions from the regex_list
     for regex_pattern in regex_list:
         cleaned_text = re.sub(regex_pattern, '', cleaned_text)
     
     return cleaned_text
 
 def clone_and_clean_files(folder_path, regex_list):
-    # Verificar si la ruta de la carpeta es válida
+    # Check if the folder path is valid
     if not os.path.exists(folder_path):
-        print("La ruta de la carpeta no es válida.")
+        print("Invalid folder path.")
         return
     
-    # Crear una nueva carpeta con el sufijo "_cleaned"
+    # Create a new folder with the suffix "_cleaned"
     new_folder_path = folder_path + "_cleaned"
     os.makedirs(new_folder_path, exist_ok=True)
     
-    # Obtener la lista de archivos en la carpeta
+    # Get the list of files in the folder
     file_list = os.listdir(folder_path)
     
     for file_name in file_list:
-        # Comprobar si el elemento es un archivo (no un directorio)
+        # Check if the element is a file (not a directory)
         file_path = os.path.join(folder_path, file_name)
         if os.path.isfile(file_path):
-            # Leer el contenido del archivo
+            # Read the content of the file
             with open(file_path, 'r', encoding='latin-1') as file:
                 text = file.read()
             
-            # Limpiar el texto usando la función clean_text
+            # Clean the text using the clean_text function
             cleaned_text = clean_text(text, regex_list)
             
-            # Crear el nuevo archivo con el sufijo "_cleaned"
+            # Create the new file with the suffix "_cleaned"
             new_file_name = os.path.splitext(file_name)[0] + "_cleaned" + os.path.splitext(file_name)[1]
             new_file_path = os.path.join(new_folder_path, new_file_name)
             
-            # Escribir el texto limpio en el nuevo archivo
+            # Write the clean text into the new file
             with open(new_file_path, 'w', encoding='latin-1') as new_file:
                 new_file.write(cleaned_text)
 
 if __name__ == "__main__":
-    # Pedir al usuario la ruta de la carpeta y las expresiones regulares
-    folder_path = input("Ingrese la ruta de la carpeta: ")
-    regex_list = input("Ingrese las expresiones regulares separadas por comas: ").split(',')
+    # Ask the user for the folder path and regular expressions
+    folder_path = input("Enter the folder path: ")
+    regex_list = input("Enter the regular expressions separated by commas: ").split(',')
     
-    # Llamar a la función para clonar y limpiar los archivos
+    # Call the function to clone and clean the files
     clone_and_clean_files(folder_path, regex_list)
-    print("Proceso completado.")
+    print("Process completed.")
